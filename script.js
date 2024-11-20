ec53258365b5ac99ddf4acddaa1c655192f75101
vinnietito <vincentnjuguna69@gmail.com>
2024-11-20 09:31:20 +0300

    Commits

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ const  dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 const today = dayOfWeek[currentDate.getDay()]
 const month = currentDate.getMonth() + 1;
 const year = currentDate.getFullYear();
+
+function getLocalTime() {
+    // Create a new Date Object
+    const currentDate =  new Date();
+
+    // Get the hours, minutes and seconds
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+
+    //Adding leading zero to minutes and seconds if they are less than 10
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    // Determine AM or PM
+    const ampm = hours >=12 ? 'PM' : 'AM';
+
+    //Convert 24-hour format ro 12 hour format
+    hours = hours % 12 || 12; //If hour equals 0, set it to 12
+
+    //Format the time
+    const localTime = `${hours}:${minutes} ${ampm}`;
+
+    return localTime;
+}
