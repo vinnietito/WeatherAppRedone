8bbbc11bda5851e37f7e12f6cf94b58ded83df6f
vinnietito <vincentnjuguna69@gmail.com>
2024-11-20 09:06:40 +0300

    Commits

---
+++ script.js
@@ -0,0 +1,5 @@
+const submitBtn = document.querySelector('#submit');
+const searchedWord = document.querySelector('#search');
+const cityTitle = document.querySelector('.city-title');
+const cityDate = document.querySelector('.city-date');
+const localTime = document.querySelector('.local-time');
